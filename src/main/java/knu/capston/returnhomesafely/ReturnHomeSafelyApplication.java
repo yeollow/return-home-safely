3ad8594272c7da3a8dcac13834ff7f2bcc298975
yeollow <ljy2784437@gmail.com>
2021-04-09 17:42:47 +0900

    Spring Initializer

---
+++ src/main/java/knu/capston/returnhomesafely/ReturnHomeSafelyApplication.java
@@ -0,0 +1,13 @@
+package knu.capston.returnhomesafely;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReturnHomeSafelyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReturnHomeSafelyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/knu/capston/returnhomesafely/ReturnHomeSafelyApplicationTests.java
@@ -0,0 +1,13 @@
+package knu.capston.returnhomesafely;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReturnHomeSafelyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
