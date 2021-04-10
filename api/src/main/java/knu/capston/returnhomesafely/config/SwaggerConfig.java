57b9baecb63a6382dcd1409a0224612160655bd1
yeollow <ljy2784437@gmail.com>
2021-04-11 02:52:56 +0900

    swagger config

---
+++ api/src/main/java/knu/capston/returnhomesafely/config/SwaggerConfig.java
@@ -0,0 +1,22 @@
+package knu.capston.returnhomesafely.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())        //Extracts the list of all URLs currently assigned to RequestMapping. There are many options except any()
+                .paths(PathSelectors.ant("/api/**"))        //Filter only URLs containing /api/**.
+                .build();
+    }
+}
