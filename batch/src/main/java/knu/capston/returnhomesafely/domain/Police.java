94f79ea0a9c3669c8dd45af6a4b12041f45f963d
yeollow <ljy2784437@gmail.com>
2021-05-10 21:41:41 +0900

    Spring Batch Job : Kafka local Producer(CCTV / Police) developing

--- batch/src/main/java/knu/capston/returnhomesafely/domain/Police.java
+++ batch/src/main/java/knu/capston/returnhomesafely/domain/Police.java
@@ -16,4 +16,18 @@ public class Police {
     @Id
     @GeneratedValue
     private Long id;
+    private String location;
+    private String number;
+    private Double latitude;
+    private Double longitude;
+    private String dataDate;
+
+    private String managementAgency;
+    private String loadLocation;
+    private String purpose;
+    private int numOfCamera;
+    private int pixels;
+    private String direction;
+    private int storageDays;
+    private String installationDate;
 }
